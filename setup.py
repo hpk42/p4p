7c50115c2e9b9b3c70bb15a676aaa75f9f985278
holger krekel <holger@merlinux.eu>
2014-03-20 08:37:25 +0100

    empty shells

---
+++ setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+import os, sys
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    here = os.path.abspath(".")
+    README = open(os.path.join(here, 'README.rst')).read()
+
+    install_requires = ["webob",
+                        "python-gnupg"]
+    if sys.version_info < (2,7):
+        install_requires.append("argparse>=1.2.1")
+
+    setup(
+      name="friendsecure",
+      description="lookup service for p2p messenger",
+      long_description=README,
+      version='0.1',
+      maintainer="Holger Krekel",
+      maintainer_email="holger@merlinux.eu",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      license="MIT",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ] + [
+            ("Programming Language :: Python :: %s" % x) for x in
+                "2.7 3.3".split()],
+      install_requires=install_requires,
+      entry_points = {
+        'console_scripts':
+                    ["friend_lookup = friend_lookup:main"],
+      })
+
