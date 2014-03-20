b7f0257c2df5fd95f2334919e6950d349f6a6066
Nicholas H.Tollervey <ntoll@ntoll.org>
2014-03-20 16:26:16 +0000

    Added test_message_received to Node tests

--- tests/test_node.py
+++ tests/test_node.py
@@ -4,7 +4,33 @@ import json
 
 
 def test_message_received():
-    pass
+    class Screen(object):
+        def __init__(self):
+            self.addLine = MagicMock()
+
+    screen = Screen()
+
+    class Peer(object):
+        def __init__(self, host='127.0.0.1', port=8888):
+            self.host = host
+            self.port = port
+
+    protocol = MagicMock()
+    protocol.transport = MagicMock()
+    protocol.transport.getPeer = MagicMock(return_value=Peer())
+
+    nod = Node('public_key', 'private_key', screen)
+    host = '127.0.0.1'
+    port = 8888
+    nod._contacts[(host, port)] = protocol
+
+    msg = {
+        'type': 'message',
+        'message': 'hello'
+    }
+    nod.message_received(msg, protocol)
+    assert 1 == nod._screen.addLine.call_count
+    assert '[THEM] hello\n' == nod._screen.addLine.call_args[0][0]
 
 
 def test_send_message():
