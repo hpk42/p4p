d397fa4f07fb6644624aab2bdace338279aa3c14
holger krekel <holger@merlinux.eu>
2014-03-20 11:00:52 +0100

    document post/get requests and how clients are supposed to use it. Also factor out the verification functions (to be implemented)

--- friend_lookup_server.py
+++ friend_lookup_server.py
@@ -1,4 +1,40 @@
+"""
+web storage for posting and getting data for a given pubkeyid.
 
+The following requests are handled:
+
+    POST /PUBKEYID (content-type=application-json)
+        {"pubkey": FULL_PUBLIC_KEY,  # must match pubkeyid,
+         "data": json_blob,
+         "data_sig": signature for data blob,
+        }
+
+    GET /PUBKEYID (reply will have content-type=application-json)
+        same content as previously posted
+
+A client will typically do the following to POST new information:
+
+    data = # some data structure
+    data_blob = json.dumps(data)
+    data_sig = sign_data(data_blob, key)
+    POST(URL + pubkeyid, data=json.dumps(
+        {"pubkey": full_pubkey,
+         "data_blob": data_blob,
+         "data_sig": data_sig
+        }
+    )
+
+and it will do the following to GET and verify information:
+
+    r = GET(URL + pubkeyid)
+    json = r.json()
+    data_blob = json["data_blob"]
+    pubkey = json["pubkey"]
+    verify_pubkeyid_belongs_to_pubkey(pubkeyid, pubkey)
+    verify_data_sig(data_blob, data_sig, pubkey)
+    data = # the data structure posted above.
+
+"""
 import sys
 import argparse
 from simplejson import loads, dumps
@@ -28,7 +64,7 @@ def make_app():
 
 class JsonRpcApp(object):
     def __init__(self):
-        self._pubkeyid_to_addresses = {}
+        self._pubkeyid2data = {}
 
     def __call__(self, environ, start_response):
         req = Request(environ)
@@ -38,28 +74,38 @@ class JsonRpcApp(object):
                 raise exc.HTTPMethodNotAllowed(
                     "method %r not allowed" % req.method)
                     #allowed='POST')
-            resp = meth(req)
+            pubkeyid = req.path.split("/")[-1]
+            resp = meth(req, pubkeyid)
         except ValueError, e:
             resp = exc.HTTPBadRequest(str(e))
         except exc.HTTPException, e:
             resp = e
         return resp(environ, start_response)
 
-    def POST(self, req):
+    def POST(self, req, pubkeyid):
         # XXX verify signature
         json = loads(req.body)
-        pubkeyid = req.path.split("/")[-1]
-        self._pubkeyid_to_addresses[pubkeyid] = json
+        self.verify_signed_json_presence(pubkeyid, json)
+        self._pubkeyid2data[pubkeyid] = json
         return Response(status=200)
 
-    def GET(self, req):
+    def GET(self, req, pubkeyid):
         # XXX also transport back signature
-        pubkeyid = req.path.split("/")[-1]
-        addresses = self._pubkeyid_to_addresses[pubkeyid]
+        data = self._pubkeyid2data[pubkeyid]
         resp = Response(
             content_type='application/json',
-            body=dumps(dict(result=addresses, error=None)))
+            body=dumps(dict(result=data, error=None)))
         return resp
 
-if __name__ == '__main__':
-    main()
+    _JSON_KEYS = set(("pubkey", "data_blob", "data_sig"))
+
+    def verify_signed_json_presence(self, pubkeyid, json):
+        if not set(json.keys()) == self._JSON_KEYS:
+            raise exc.HTTPBadRequest(
+                "json must have these keys: %s" %(self._JSON_KEYS))
+        pubkey = json["pubkey"]
+        data_blob = json["data_blob"]
+        data_sig = json["data_sig"]
+        #verify_data_integrity(pubkeyid, pubkey, data, data_sig)
+        #verify that pubkeyid fits to pubkey and that data_sig is a valid
+        #signature for data
--- test_friend_lookup_server.py
+++ test_friend_lookup_server.py
@@ -3,13 +3,21 @@ from webob import Request
 from friend_lookup_server import make_app
 from simplejson import dumps, loads
 
-def test_post_and_get():
-    req = Request.blank('http://localhost/12345678', method="POST")
+def test_post_invalid():
     app = make_app()
+    req = Request.blank('http://localhost/12345678', method="POST")
     data = {"hello": 42}
+    r = req.get_response(app)
+    assert r.status_code == 400
+
+def test_post_and_get():
+    app = make_app()
+    req = Request.blank('http://localhost/12345678', method="POST")
+    data = dict(pubkey="123", data_blob="456", data_sig="102938")
     req.body = dumps(data)
     r = req.get_response(app)
     assert r.status_code == 200
+
     req = Request.blank("http://localhost/12345678", method="GET")
     r = req.get_response(app)
     assert r.status_code == 200
