85c3f8f0e74cb5edcc2cc8fc81e035217abaed97
holger krekel <holger@merlinux.eu>
2014-03-20 17:27:56 +0100

    add missing file (doh)

---
+++ friendsecure/contacts.py
@@ -0,0 +1,14 @@
+
+from json import dump, load
+
+class Contacts:
+    def __init__(self, contactfile):
+        with open(contactfile, "rb") as f:
+            self._contacts = load(f)
+
+    def get_fingerprint(self, nick_or_fingerprint):
+        for fingerprint, val in self._contacts.items():
+            if fingerprint == nick_or_fingerprint:
+                return fingerprint
+            elif val["nick"] == nick_or_fingerprint:
+                return fingerprint
